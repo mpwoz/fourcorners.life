6da4163c8eb51e64a7c8058426fd44044777e39e
Martin Wozniewicz <martin.woz@gmail.com>
2019-05-05 14:16:45 -0700

    flashy splash screen background

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,52 @@
+import { graphql, StaticQuery } from "gatsby";
 import PropTypes from "prop-types"
 import React from "react"
 
 import { Instagram } from 'react-feather'
 
 import logo from "../images/logo.jpg";
-
+import BackgroundImage from 'gatsby-background-image'
 
 import './header.scss';
+import Image from "./image";
 
 const Logo = (props) => (<img className="logo-image"
                               {...props} />);
+
+
+const BackgroundSection = ({className, children}) => (
+  <StaticQuery query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "tina.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+               render={data => {
+                 // Set ImageData.
+                 const imageData = data.desktop.childImageSharp.fluid;
+                 return (
+                   <BackgroundImage Tag="section"
+                                    className={className}
+                                    fluid={imageData}
+                                    backgroundColor={`#040e18`}
+                   >
+                     {children}
+                   </BackgroundImage>
+                 )
+               }}
+  />
+);
+
 
 const Header = ({siteTitle}) => (
   <>
     <header>
       <div className="container">
-        {/*<div className="navbar-brand is-size-1">*/}
-        {/*<Logo src={logo}/>*/}
-        {/*{siteTitle}*/}
-        {/*</div>*/}
         <div className="navbar-menu">
           <div className="navbar-start">
             <a className="navbar-item"> About </a>
@@ -36,19 +64,20 @@ const Header = ({siteTitle}) => (
 
       </div>
     </header>
-    <section class="hero is-primary">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title is-size-2">
-        Four Corners
-      </h1>
-      <h2 class="subtitle">
-        Yoga. Nutrition. Coaching.
-      </h2>
-    </div>
-  </div>
-</section>
-
+    <section className="hero is-primary splash-img">
+      <BackgroundSection>
+        <div className="hero-body" style={{minHeight: `440px`}}>
+          <div className="container has-text-centered" style={{background: `#000c`}}>
+            <h1 className="title is-size-2">
+              Four Corners
+            </h1>
+            <h2 className="subtitle">
+              Yoga. Nutrition. Coaching.
+            </h2>
+          </div>
+        </div>
+      </BackgroundSection>
+    </section>
 
   </>
 )
--- src/components/image.js
+++ src/components/image.js
@@ -17,9 +17,9 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "tina.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxHeight: 6600) {
               ...GatsbyImageSharpFluid
             }
           }
