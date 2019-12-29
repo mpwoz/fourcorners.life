e734280ec49c6e14d7259009457b96a55b55abb9
Martin Wozniewicz <martin.woz@gmail.com>
2019-12-29 11:16:10 -0800

    Basic working blog with routing and sample posts.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,13 +4,43 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const BlogPage = () => (
+const toBlogPostList = allMarkdownRemark =>
+  <ul>
+    {allMarkdownRemark.edges
+      .map(({node}) => (
+        <li>
+          <Link to={node.fields.slug}>
+            {node.frontmatter.title}
+          </Link>
+        </li>
+      ))}
+  </ul>;
+
+const BlogPage = ({data, location}) => (
   <Layout>
-    <SEO title="Blog" />
-    <h1>Blog</h1>
-    <p>A series of posts written by me.</p>
+    <SEO title="Blog"/>
+    <h1>Blog posts:</h1>
+    {toBlogPostList(data.allMarkdownRemark)}
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
+
+export const pageQuery = graphql`
+query Blog {
+  allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+    edges {
+      node {
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+        }
+      }
+    }
+  }
+}
+`;
 
 export default BlogPage
